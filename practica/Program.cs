01aefdb2fb344413510fd5d0c4f4da9f58b01bc0
Darlink <darlinkaguilargr@gmail.com>
2025-08-27 22:16:50 -0500

    Creacion del codigo

---
+++ practica/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var estudiante1 = new Estudiantes();
+estudiante1.Id = 1;
+estudiante1.Nombre = "Darlink";
+estudiante1.Actividad = true;
+estudiante1.Estatura = 1.52m;
+estudiante1.Nacimiento = DateTime.Now;
+estudiante1.Promedio = 4.5m;
+
+Console.WriteLine(estudiante1.Id);
+Console.WriteLine(estudiante1.Nombre);
+
+public class Personas
+{
+    public int Id;
+    public string? Nombre;
+    public bool Actividad;
+    public decimal Estatura;
+    public DateTime Nacimiento;
+}
+
+public class Estudiantes : Personas
+{
+    public decimal Promedio;
+}
